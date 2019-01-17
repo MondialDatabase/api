828156dd353c50f6d05d9e0588f0a309fd803cb8
Giuseppe Scoppino <scoppino.giuseppe@gmail.com>
2019-01-17 17:41:36 -0500

    Cache Maven repository between builds that are done with the disposable Docker Maven container.

--- build-api.sh
+++ build-api.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
+# Ensure a re-usable Docker volume for the Maven repository exists for caching purposes
+docker volume create --name mondial-api-builder-repository
+
 # Build the WAR file in a disposable Docker container
-docker run --rm --name mondial-api-builder -v "$(pwd)":/usr/src/mymaven -w /usr/src/mymaven maven:3-jdk-11 mvn clean package
+docker run --rm --name mondial-api-builder -v mondial-api-builder-repository:/root/.m2 -v "$(pwd)":/usr/src/mymaven -w /usr/src/mymaven maven:3-jdk-11 mvn clean package
