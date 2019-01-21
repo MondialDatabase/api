b54938f5ae92928b367617a32257660138953cca
Giuseppe Scoppino <scoppino.giuseppe@gmail.com>
2019-01-21 14:52:00 -0500

    Improve Docker image/volume/network naming schemes. Most importantly the network name is now static since it is no longer based on the name of the project folder.

--- test-api.sh
+++ test-api.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Ensure a re-usable Docker volume for the Maven repository exists for caching purposes
-docker volume create --name mondial-api-builder-repository
+docker volume create --name mondial-api-maven-repository
 
 # Run the tests in a disposable Docker container
-docker run --rm --name mondial-api-tester --network=mondial-server_default -v mondial-api-builder-repository:/root/.m2 -v "$(pwd)":/usr/src/mymaven -w /usr/src/mymaven maven:3-jdk-11 mvn test -Djersey.test.host=api-server
+docker run --rm --name mondial-api-tester --network=mondial -v mondial-api-maven-repository:/root/.m2 -v "$(pwd)":/usr/src/mymaven -w /usr/src/mymaven maven:3-jdk-11 mvn test -Djersey.test.host=api-server
